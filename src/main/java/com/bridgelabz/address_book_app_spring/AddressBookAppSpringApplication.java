2ce4f9198dc764531af44155ab705beda28c707e
rojajullapalli <rojajules@gmail.com>
2021-10-13 08:18:55 +0530

    ADDED: Initial commit

---
+++ src/main/java/com/bridgelabz/address_book_app_spring/AddressBookAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.address_book_app_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookAppSpringApplication.class, args);
+    }
+
+}
